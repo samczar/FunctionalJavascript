3cee1ac1999140f02348d8bf8d9642445bef6028
AnyankaChibuzo <senetor.samczar@gmail.com>
2019-03-27 09:54:20 +0100

    twoPower

--- twoPower.js
+++ twoPower.js
@@ -5,3 +5,16 @@ const show1 = array1.map(el => Math.pow(2, el));
 console.log(show1);
 //Output is
 //[ 2, 4, 8, 16, 32, 64, 128 ]
+
+var objArr = [
+  { w: 10, h: 20, d: 10 },
+  { w: 3, h: 2, d: 20 },
+  { w: 4, h: 1, d: 400 },
+  { w: 9000, h: 3999, d: 9191 }
+];
+
+objArr.map(({ w, h, d }) => {
+  //Deconstructuring
+  var val = w * h * d;
+  console.log(val);
+});
