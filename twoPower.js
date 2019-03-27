2df52420a3ace4fb3dd0c1182e7f223988d5e82c
AnyankaChibuzo <senetor.samczar@gmail.com>
2019-03-27 09:22:47 +0100

    twoPower

--- twoPower.js
+++ twoPower.js
@@ -1,3 +1,7 @@
 array1 = [1, 2, 3, 4, 5, 6, 7];
 const show = array1.map(el => 2 ** el);
-console.log(show);
+//Same in value
+const show1 = array1.map(el => Math.pow(2, el));
+console.log(show1);
+//Output is
+//[ 2, 4, 8, 16, 32, 64, 128 ]
